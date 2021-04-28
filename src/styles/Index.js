cb111d817c579ab8ab8ccec8500399dd6073462f
yHenr1que <henrique.rodrigues.oliveira@hotmail.com>
2021-04-27 21:14:36 -0300

    Primeiro commit

---
+++ src/styles/Index.js
@@ -0,0 +1,15 @@
+ 
+ 
+ const style = StyleSheet.create({
+    container: {
+      flex: 1
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    texto: {
+      color: '#000'
+    }
+    slogan {
+        color: '#000'
+    }
+  });
